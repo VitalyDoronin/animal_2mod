58f347d3fc86e978d8d7408ed2c78ad276ab8434
VitalyDoronin <doronin.v688@gmail.com>
2025-04-10 21:26:06 +0300

    Update train_main.py

--- train_models/train_main.py
+++ train_models/train_main.py
@@ -4,23 +4,22 @@ import os
 
 mlflow.set_tracking_uri("http://localhost:5050")
 def main():
-    # Enable MLflow logging
+    # Включаем ведение журнала MLflow
     # settings.update({"mlflow": True})
 
-    # Set project and run names
     os.environ["MLFLOW_EXPERIMENT_NAME"] = "YOLO_animal_proj"
     os.environ["MLFLOW_RUN"] = "YOLO_run5"
     os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
     os.environ["AWS_ACCESS_KEY_ID"] = "mlflow"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "password"
 
-    # Load the model
+    # Загружаем модель
     model = YOLO('yolo11n.pt')
 
-    # Define custom data path
+    # Указываем путь к данным
     custom_data_path = r"/Users/vi/Animals_Project/Animal/train_models/animals.v2-release.yolov11/data.yaml"
 
-    # Train the model
+    # Указываем параметры обучения модели
     results = model.train(
         data=custom_data_path,
         imgsz = 320,
